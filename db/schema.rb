6d1995c0acf9956743bc4425ea2cdfbaf078b712
Jonathan Sivahera <jsivahera@icloud.com>
2022-09-07 00:29:30 +0200

    Add user_id to groups

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2022_09_06_215943) do
+ActiveRecord::Schema[7.0].define(version: 2022_09_06_222636) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -27,6 +27,7 @@ ActiveRecord::Schema[7.0].define(version: 2022_09_06_215943) do
     t.string "icon"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.integer "user_id", null: false
   end
 
   create_table "users", force: :cascade do |t|
@@ -43,4 +44,5 @@ ActiveRecord::Schema[7.0].define(version: 2022_09_06_215943) do
   end
 
   add_foreign_key "entities", "users", column: "author_id"
+  add_foreign_key "groups", "users"
 end
