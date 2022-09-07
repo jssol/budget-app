1f3f7dfda53e2d876eb4cd0562dae2495768a849
Jonathan Sivahera <jsivahera@icloud.com>
2022-09-07 19:09:36 +0200

    Fix linters

--- app/controllers/transactions_controller.rb
+++ app/controllers/transactions_controller.rb
@@ -1,13 +1,9 @@
 class TransactionsController < ApplicationController
-  def index
-  end
+  def index; end
 
-  def show
-  end
+  def show; end
 
-  def new
-  end
+  def new; end
 
-  def create
-  end
+  def create; end
 end
