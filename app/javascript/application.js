e20f1701a721c11d03232ecf9656638f8b203dc8
Jonathan Sivahera <jsivahera@icloud.com>
2022-09-07 10:06:46 +0200

    Test animejs

--- app/javascript/application.js
+++ app/javascript/application.js
@@ -1,2 +1,11 @@
 // Configure your import map in config/importmap.rb. Read more: https://github.com/rails/importmap-rails
 import './modules/homepage'
+import anime from './modules/animejs/lib/anime.es.js'
+
+anime({
+  targets: "#test",
+  translateX: 250,
+  rotate: "1turn",
+  backgroundColor: "#FFF",
+  duration: 800,
+});
