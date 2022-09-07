8e9bac35496855e59f3422ac28419ff63e4c0db5
Jonathan Sivahera <jsivahera@icloud.com>
2022-09-07 16:09:24 +0200

    Use resource to generate RESTful routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,18 +1,12 @@
 # frozen_string_literal: true
 
 Rails.application.routes.draw do
-  get 'transactions/index'
-  get 'transactions/show'
-  get 'transactions/new'
-  get 'transactions/create'
-  get 'categories/index'
-  get 'categories/show'
-  get 'categories/new'
-  get 'categories/create'
-  get 'homes/index'
   devise_for :users
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
   root "homes#index"
+
+  resources :categories, only: [:index, :show, :create, :new]
+  resources :transactions, only: [:index, :show, :create, :new]
 end
