3f2bf61dd1f6fb765cb0e09e7e5f193d30c519cc
Jonathan Sivahera <jsivahera@icloud.com>
2022-09-07 16:01:24 +0200

    Generate Transactions && Categories resources

---
+++ app/controllers/transactions_controller.rb
@@ -0,0 +1,13 @@
+class TransactionsController < ApplicationController
+  def index
+  end
+
+  def show
+  end
+
+  def new
+  end
+
+  def create
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,14 @@
 # frozen_string_literal: true
 
 Rails.application.routes.draw do
+  get 'transactions/index'
+  get 'transactions/show'
+  get 'transactions/new'
+  get 'transactions/create'
+  get 'categories/index'
+  get 'categories/show'
+  get 'categories/new'
+  get 'categories/create'
   get 'homes/index'
   devise_for :users
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
