484df24c38a9443d30d7b8f26ed5f5a9b1eafd85
Jonathan Sivahera <jsivahera@icloud.com>
2022-09-09 17:05:46 +0200

    Delete unused module

--- app/javascript/application.js
+++ app/javascript/application.js
@@ -1,7 +1,7 @@
 // Configure your import map in config/importmap.rb. Read more: https://github.com/rails/importmap-rails
 import './modules/homepage'
 import './modules/header';
-// import anime from './modules/animejs/lib/anime.es.js'
+import "./modules/entity";
 import "trix"
 import "@rails/actiontext"
 
