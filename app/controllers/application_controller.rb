b6aa78112941dc34dde719676580b4b4185e90ea
Jonathan Sivahera <jsivahera@icloud.com>
2022-09-09 19:43:25 +0200

    Pass user id to redirection

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -4,7 +4,7 @@ class ApplicationController < ActionController::Base
   private
 
   def after_sign_in_path_for(_resource)
-    categories_path
+    user_groups_path(current_user.id)
   end
 
   def after_sign_out_path_for(_resource)
