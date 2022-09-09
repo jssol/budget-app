e44cccf378321933174764a26670c50401e6c231
Jonathan Sivahera <jsivahera@icloud.com>
2022-09-09 15:37:38 +0200

    Add control flow for the Entity model

--- app/controllers/entities_controller.rb
+++ app/controllers/entities_controller.rb
@@ -1,2 +1,48 @@
 class EntitiesController < ApplicationController
+  def index
+    @entities = Entity.where(author_id: current_user.id).where(group_id: params[:group_id])
+  end
+
+  def show
+    redirect_to user_group_entities_path(user_id: current_user.id, group_id: params[:id])
+  end
+
+  def new
+    @entity = Entity.new
+    respond_to do |format|
+      format.html { render :new, locals: { entity: @entity } }
+    end
+  end
+
+  def create
+    @entity = Entity.new(entity_params)
+    if @entity.save
+      flash[:notice] = 'Transaction added successfully'
+      redirect_to user_group_entities_path(current_user.id)
+    else
+      flash[:error] = 'Transaction could not be added'
+      render :new, locals: { entity: @entity }
+    end
+  end
+
+  def most_recent
+    @entities = Entity.where(author_id: current_user.id).where(group_id: params[:group_id]).order('created_at DESC').limit(5)
+  end
+
+  def most_ancient
+    @entities = Entity.where(author_id: current_user.id).where(group_id: params[:group_id]).order('created_at ASC').limit(5)
+  end
+
+  def most_expensive
+    @entities = Entity.where(author_id: current_user.id).where(group_id: params[:group_id]).order('amount DESC').limit(5)
+  end
+
+  private
+
+  def entity_params
+    @params = params.require(:entity).permit(:name, :amount)
+    @params[:author_id] = current_user.id
+    @params[:group_id] = params[:group_id]
+    @params
+  end
 end
