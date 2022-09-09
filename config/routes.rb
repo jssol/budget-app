9381543f4f26c7446c12dac4bfd77f5923d40370
Jonathan Sivahera <jsivahera@icloud.com>
2022-09-09 15:34:51 +0200

    Add custom routes for Entities

--- config/routes.rb
+++ config/routes.rb
@@ -7,6 +7,10 @@ Rails.application.routes.draw do
   # Defines the root path route ("/")
   root "homes#index"
 
+  get '/users/:user_id/groups/:group_id/entities/most_recent', to: 'entities#most_recent', as: 'recent_expenses'
+  get '/users/:user_id/groups/:group_id/entities/most_ancient', to: 'entities#most_ancient',  as: 'ancient_expenses'
+  get '/users/:user_id/groups/:group_id/entities/most_expensive', to: 'entities#most_expensive', as: 'expensive_expenses'
+
   resources :users, only: [:show] do
     resources :groups, only: [:index, :show, :create, :new] do
       resources :entities, only: [:index, :show, :create, :new]
