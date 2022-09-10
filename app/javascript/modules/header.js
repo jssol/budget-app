21083669ffba1cc2380fb6dfb4bb9296c802ffbb
Jonathan Sivahera <jsivahera@icloud.com>
2022-09-10 02:00:46 +0200

    Add mobile navigation functionality

--- app/javascript/modules/header.js
+++ app/javascript/modules/header.js
@@ -3,16 +3,14 @@ const pageBody = document.getElementsByClassName('body')[0];
 
 if (location.pathname.match(/^\/users\/\d+\/groups$/) || location.pathname.match(/^\/users\/\d+\/group\/entities+$/)) {
   const hamburgerButton = document.querySelector(".hamburger-button");
+  const hamburgerCloser = document.querySelector(".hamburger-closer");
   const hamburger = document.querySelector(".hamburger");
 
   hamburgerButton.addEventListener("click", () => {
-    page.classList.toggle("nav-open");
-    if (page.classList.contains("nav-open")) {
-      hamburger.classList.remove("fa-bars");
-      hamburger.classList.add("fa-times");
-    } else {
-      hamburger.classList.remove("fa-times");
-      hamburger.classList.add("fa-bars");
-    }
+    page.classList.add("nav-open");
+  });
+
+  hamburgerCloser.addEventListener("click", () => {
+    page.classList.remove("nav-open");
   });
 }
